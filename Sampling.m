b3e32b1664775d8f79105a76dae574b12e246364
DongUk_Kim <g9896@kpu.ac.kr>
2020-07-15 00:10:33 +0900

    Update Sampling.m

--- Sampling.m
+++ Sampling.m
@@ -8,18 +8,24 @@ t= linspace(0,10,N);
 freq = 1;
 w = 2*pi*freq;
 
-ts = 10;% sampling time
-ts = (ts*10)/N
+fs = 4*pi*freq;% sampling frequency
+ts = N*(1/10*(1/fs))
 
 impulse_train = zeros(1, N); 
 impulse_train(1:ts:end) = 1
 
+stem(t,impulse_train);
+hold on
+
 analog_signal = sin(w*t);
+
 plot(t,analog_signal);
 grid on
-digital_signal = impulse_train .* analog_signal; 
-stem(t,digital_signal);
 
+digital_signal = impulse_train .* analog_signal; 
+
+figure; stem(t, digital_signal);
+grid on
 
 
 
